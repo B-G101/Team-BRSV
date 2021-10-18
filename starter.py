fa7a71921b42c5be81680a65c00ada93fe6ba49a
briag <briagee101@gmail.com>
2021-10-17 22:26:54 -0700

    fixed random generator error

--- starter.py
+++ starter.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+import requests
 
 from pathlib import \
     Path  # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
