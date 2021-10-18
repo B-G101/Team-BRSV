750e49b22d1850604634d56c40c72cd8d01b5fe0
Sreeja Vadlamudi <89220356+SreejaVad@users.noreply.github.com>
2021-10-18 03:44:54 -0700

    Edits in format and change of instructions

--- starter.py
+++ starter.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, render_template
-import requests
 
 from pathlib import \
-    Path  # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
-
+    Path  # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b5
 app_starter = Blueprint('starter', __name__,
                         url_prefix='/starter',
                         template_folder='templates',
