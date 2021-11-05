b3da43ce55da68b22cd39a9940c58116ddb9653a
DistilledVinegar <ericw5845@outlook.com>
2021-11-04 19:57:26 -0700

    twitter api work

--- static/twitter.js
+++
@@ -1,5 +0,0 @@
-let tweets = new XMLHttpRequest();
-
-XMLHttpRequest.setRequestHeader(tweets, AAAAAAAAAAAAAAAAAAAAAPWmVAEAAAAAMEiAEe%2BMjldaoVlSHaW7d34sy14%3DYhZhEx9hUnUNSFa3XZZBBo7dqJ1hgzdBr8PprxUFb6izm9loki)
-tweets.open("GET", "https://api.twitter.com/2/users/1350144649546006534/tweets")
-
